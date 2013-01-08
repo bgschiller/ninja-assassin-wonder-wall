4e39170b29ba1ef8e55fe91dd3d865e464f0ab19
Brian Schiller <bgschiller@gmail.com>
2013-01-08 12:32:56 -0800

    reworked random_games to allow enumeration of all games. (still a naive implementation, which doesn't distinguish between two isomorphic games) added .gitignore file

--- random_games.py
+++ random_games.py
@@ -11,11 +11,32 @@ def random_game(players):
         relationships.append((p,ww,na))
     return relationships
 
-def count_solvable(n_players, n_samples):
+
+def combinations(lst, so_far=[]):
+    if len(lst) == 0:
+        return [so_far]
+    head = lst[0]
+    tail = lst[1:]
+    combo_list = []
+    for elem in head:
+        combo_list.extend(combinations(tail, so_far + [elem]))
+    return combo_list
+
+def all_games(players):
+    choice_lists = []
+    for p in players:
+        choices = []
+        for ww, na in ((w, n) for w in players for n in players 
+                if (not w == p and not n == p and not n == w)):
+            choices.append((p,ww,na))
+        choice_lists.append(choices)
+    return combinations(choice_lists)
+
+def count_solvable(game_list):
     n_solvable = 0
-    for i in range(n_samples):
+    for relationships in game_list:
         outfile = StringIO()
-        if find_ordering(random_game(range(n_players)), outfile) is not None:
+        if find_ordering(relationships, outfile) is not None:
             n_solvable += 1
         outfile.close()
     return n_solvable
@@ -26,4 +47,4 @@ if __name__ == '__main__':
     print '{0:4<} {1:8<}'.format('n', 'P(solvable)')
     for game_size in range(4,max_size):
         print '{0:4<} {1:8<1.6f}'.format(game_size, 
-                float(count_solvable(game_size, n_samples))/ n_samples)
+                float(count_solvable((random_game(range(game_size)) for x in xrange(n_samples))))/ n_samples)

063eeca216b0128aaf92dd40d3f6a43587cd1f2a
Brian Schiller <bgschiller@gmail.com>
2013-01-22 00:01:54 -0800

    Changed all_games to generator function Wrote a theorem that all solutions can be expressed in 1-dim find_ordering now returns the recursion depth at which the solution was found.

--- implication.py
+++ implication.py
@@ -34,7 +34,7 @@ def add_preds(g, x, queue=None, preds=[]):
         add_preds(g,succ, queue, preds + [x])
     
 def make_assumption(pair, pairs_count, given_graph, relationships, outfile,
-        reverse_checked=False, spaces=2):
+        reverse_checked=False, depth=1):
     """Given an edge to start with, make_assumption fixes that edge in the given orientation.
     Then it fixes all the edges implied by this orientation. If this produces a DAG, recurse 
     on another edge. Otherwise, try the opposite orientation.
@@ -47,10 +47,10 @@ def make_assumption(pair, pairs_count, given_graph, relationships, outfile,
     relationships -- a list of 3-tuples (x,y,z) where y is x's WW and z is x's NA
     reverse_checked -- if true, we have already concluded that the opposite orientation 
     leads to a cyclic graph, so if this orientation doesn't work out, there is no solution.
-    spaces -- should not be set at top level call. spaces determines the indentation level, 
-    so that the output visually represents the recursion depth."""
+    depth -- should not be set at top level call. depth determines the indentation level, 
+    so that the output visually represents the recursion depth. It is also used as a metric to evaluate the algorithm's performance"""
 
-    outfile.write( '{spaces}Assume {u} -> {v}:\n'.format(spaces=' '*(spaces-2), u=pair[0], v=pair[1]))
+    outfile.write( '{spaces}Assume {u} -> {v}:\n'.format(spaces=' '*(2*(depth-1)), u=pair[0], v=pair[1]))
     working_graph = given_graph.copy()
     to_process = deque([pair])
     working_count = pairs_count.copy()
@@ -70,7 +70,7 @@ def make_assumption(pair, pairs_count, given_graph, relationships, outfile,
                 to_add = (y,x)
             if to_add is not None and (to_add not in working_graph.edges_iter()):
                 outfile.write( '{spaces}{a} -> {b} is given, so relationship {xyz} => {w} -> {u}\n'.format(
-                        spaces=' '*spaces, a=a, b=b, xyz=(x,y,z), w=to_add[0], u=to_add[1]))
+                        spaces=' '*(2*(depth-1)), a=a, b=b, xyz=(x,y,z), w=to_add[0], u=to_add[1]))
                 working_graph.add_edge(*to_add)
                 to_process.append(to_add)
         if nx.is_directed_acyclic_graph(working_graph):
@@ -84,18 +84,18 @@ def make_assumption(pair, pairs_count, given_graph, relationships, outfile,
 
     if nx.is_directed_acyclic_graph(working_graph):
         if len(working_graph.nodes()) == len(relationships):
-            return True, working_graph #success!
+            return True, working_graph, depth #success!
         next_pair = sorted(working_count.most_common(1)[0][0])#no contradictions, but not done yet
         return make_assumption(next_pair, working_count, working_graph, relationships,
-                outfile, spaces=spaces+2)
+                outfile, depth=depth+1)
     elif not reverse_checked: #that didn't work out... check the other side!
-        outfile.write( '{spaces}Cycle in graph! try the reverse...\n'.format(spaces=' '*spaces))
+        outfile.write( '{spaces}Cycle in graph! try the reverse...\n'.format(spaces=' '*(2*depth)))
         return make_assumption(list(reversed(pair)), pairs_count, given_graph, 
-                relationships, outfile, reverse_checked=True, spaces=spaces)
+                relationships, outfile, reverse_checked=True, depth=depth)
     else:
         outfile.write('{spaces}Cycle in graph, and we\'ve already checked the other side! Returning false.\n'.format(
-                spaces=' '*spaces))
-        return False, working_graph
+                spaces=' '*2*depth))
+        return False, working_graph, depth
 
 def count_pairs(relationships):
     """relationships should be a list of 3-tuples (x,y,z). return a Counter
@@ -113,24 +113,24 @@ def find_ordering(relationships, outfile=sys.stdout):
     pair_count = count_pairs(relationships)
     starting_pair = pair_count.most_common(1)[0][0]
     order_graph = nx.DiGraph()
-    solvable, order_graph = make_assumption(starting_pair, pair_count, order_graph, 
-            relationships, outfile)
+    solvable, order_graph, recursion_depth = make_assumption(starting_pair, pair_count, 
+            order_graph, relationships, outfile)
     if solvable:
         order = nx.topological_sort(order_graph)
         outfile.write('solution: {0}\n'.format(order))
-        return order
+        return order, recursion_depth
     else:
         outfile.write('no solution.\n')
         for cycle in nx.simple_cycles(order_graph):
             outfile.write('cycle: {0}\n'.format(' -> '.join(map(str, cycle))))
-        return None
+        return None, recursion_depth
 
 if __name__ == '__main__':
     relationships = [('a','b','c'),('b','c','d'),('c','b','a'),('d','b','a'),('e','a','d'),
             ('f','c','e')]
     print 'is there a solution to {0}?'.format(relationships)
     find_ordering(relationships)
-    raw_input()
+    print ''
     relationships = [(1,2,3),(2,4,6),(3,4,7),(4,1,5),(5,6,3),(6,1,7),(7,2,5)]
     print 'is there a solution to {0}?'.format(relationships)
     find_ordering(relationships)
--- random_games.py
+++ random_games.py
@@ -23,28 +23,40 @@ def combinations(lst, so_far=[]):
     return combo_list
 
 def all_games(players):
-    choice_lists = []
     for p in players:
         choices = []
         for ww, na in ((w, n) for w in players for n in players 
                 if (not w == p and not n == p and not n == w)):
             choices.append((p,ww,na))
-        choice_lists.append(choices)
-    return combinations(choice_lists)
+        yield choices
 
 def count_solvable(game_list):
     n_solvable = 0
+    solve_depths = []
+    unsolve_depths = []
     for relationships in game_list:
         outfile = StringIO()
-        if find_ordering(relationships, outfile) is not None:
+        ordering, recursion_depth = find_ordering(relationships, outfile)
+        if ordering is not None:
             n_solvable += 1
+            solve_depths.append(recursion_depth)
+        else:
+            unsolve_depths.append(recursion_depth)
         outfile.close()
-    return n_solvable
+    return n_solvable, solve_depths, unsolve_depths
 
 if __name__ == '__main__':
     n_samples = 1000
-    max_size = 10
-    print '{0:4<} {1:8<}'.format('n', 'P(solvable)')
+    max_size = 20
+    print 'd_s: recursion depth for solvable problems'
+    print 'd_u: recursion depth for unsolvable problems'
+    print '{0:>4} {1:>12} {2:>12} {3:>12} {4:>12} {5:>12}'.format('n', 'P(solvable)', 'avg(d_s)', 'max(d_s)', 'avg(d_u)', 'max(d_u)')
     for game_size in range(4,max_size):
-        print '{0:4<} {1:8<1.6f}'.format(game_size, 
-                float(count_solvable((random_game(range(game_size)) for x in xrange(n_samples))))/ n_samples)
+        n_solveable, solve_depths, unsolve_depths = count_solvable(
+            (random_game(range(game_size)) for x in xrange(n_samples)))
+        print '{0:>4} {1:>1.10f} {2:>1.10f} {3:>12} {4:>1.10f} {5:>12}'.format(game_size, 
+                float(n_solveable)/ n_samples, 
+                sum(solve_depths)/float(len(solve_depths)),
+                max(solve_depths),
+                sum(unsolve_depths)/float(len(unsolve_depths)),
+                max(unsolve_depths))

76c932e142426d67624c038940a5573bf31e4240
Brian Schiller <bgschiller@gmail.com>
2013-03-15 16:59:42 -0700

    Python client to spin up the c++ implementation to solve problems

---
+++ cpp_implementation/naww_client.py
@@ -0,0 +1,47 @@
+'''designed to be called over ssh by driver.py
+should pass along as command line parameters the location of a redis instance
+and the number of children to maintain at a time.
+The redis instance will include a list 'jobs', each entry of which will be of the form
+n p1 w1 a1 p2 w2 a2 ...
+where n is the number of players,
+pi is the ith player
+wi is the ith player's wonderwall
+ai is the ith player's ninja assassin.
+These are to be fed as input to the c++ implementation.
+If there is a solution, it should be set as the value for key 
+n p1 w1 a1 p2 w2 a2 ...
+and the key should added to 'solved:n' (for appropriate n).
+If there is no solution, add the key to 'unsolvable:n' (for appropriate n).'''
+
+
+import multiprocessing
+import subprocess
+import redis
+
+r = redis.Redis()
+
+def find_solution(problem):
+    '''solve a single problem, presented as a string:
+    n p1 w1 a1 p2 w2 a2 ...
+    and push the results to redis'''
+    solution = subprocess.check_output("echo {} | ./implication".format(problem))
+    size = int(problem.split()[0]) 
+    if solution:
+        r.sadd('solved:{}'.format(size), problem)
+        r.set('game:{}'.format(problem), solution)
+    else:
+        r.sadd('unsolvable:{}'.format(size),problem)
+
+def problems():
+    '''This generator will continue to produce values for the pool until 
+    there are no more in the redis list'''
+    new_prob = r.rpoplrpush('jobs','inprogress')
+    while new_prob is not None:
+        yield new_prob
+        new_prob = r.rpoplpush('jobs','inprogress')
+
+
+num_cpus = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(num_cpus * 2)
+pool.map_async(find_solution,problems()).wait()
+

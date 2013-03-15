235b1652632d625357bb5494c1b6eb6335faea42
Brian Schiller <bgschiller@gmail.com>
2013-03-15 16:58:11 -0700

    Fixed up implication to take input from stdin (also, it works now)

--- cpp_implementation/implication.cpp
+++ cpp_implementation/implication.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
 #include<stdlib.h>
+#include<queue>
 #include <boost/utility.hpp>
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/depth_first_search.hpp>
 #include <boost/graph/visitors.hpp>
 #include <boost/graph/topological_sort.hpp>
+#include <boost/graph/lookup_edge.hpp>
 using namespace boost;
 using namespace std;
 
-//so that we can change the defaults
+
+#ifdef DEBUG
+#define SPACES(x) for(int ii=0; ii<2*x; ii++) printf(" ");
+#define OUTPUT(x) printf x
+#else
+#define SPACES(x) 
+#define OUTPUT(x) 
+#endif
+//programmingexamples.net/wiki/Bost/BGL/CreateGraph
+//typedef so that we can change this later
 typedef adjacency_list<> graph_t;
 typedef struct {
     int p;
@@ -17,12 +28,11 @@ typedef struct {
     int n;
 } player_t;
 typedef pair<int,int> Edge;
-typedef graph_traits<Graph>::vertex_descriptor Vertex;
 
 /* function prototypes */
 vector<Edge> ImpliedEdges(Edge guess, const vector<player_t>& players);
-void AddImpliedEdges(graph_t& F, Edge guess, const vector<player_t>& players);
-pair<bool,graph_t> MakeAssumption(graph_t& G, vector<player_t> players);
+void AddImpliedEdges(graph_t& F, Edge guess, const vector<player_t>& players, unsigned short depth);
+pair<bool,graph_t> MakeAssumption(graph_t& G, vector<player_t> players, unsigned short depth=0);
 
 struct cycle_detector : public dfs_visitor<> //code from boost/graph/example/file_dependencies.cpp
 {
@@ -38,25 +48,29 @@ bool is_acyclic(const graph_t& G)
     bool has_cycle = false;
     cycle_detector vis(has_cycle);
     depth_first_search(G, visitor(vis));
-    return has_cycle;
+    return ! has_cycle;
 }
 
 
 
-vector<Vertex> FindOrdering(vector<player_t> players){
-    graph_t order_g();
-    pair<bool,graph_t> result = MakeAssumption(order_g, players);
-    vector<Vertex> order;
-    if (result.first){
-        topological_sort(result.second, front_inserter(order));
+vector<int> FindOrdering(vector<player_t> players){
+    graph_t order_g;
+    bool acyclic;
+    OUTPUT(("inside FindOrdering\n"));
+    add_edge(1,2,order_g);      //BGL causes a segfault if we ask edge() on an empty group
+    remove_edge(1,2,order_g);   //(2nd part of hack)
+    boost::tie(acyclic,order_g) =  MakeAssumption(order_g, players);
+    vector<int> order;
+    if (acyclic){
+        topological_sort(order_g, back_inserter(order));
     }
     return order;
 }
 
 vector<Edge> ImpliedEdges(Edge guess, const vector<player_t>& players){
-    vector<Edge> impl_edges();
-    Vertex rr = guess.first;
-    Vertex ss = guess.second;
+    vector<Edge> impl_edges;
+    int rr = guess.first;
+    int ss = guess.second;
     for (size_t ii = 0; ii < players.size(); ++ii){
         int tt = players[ii].p;
         int ww = players[ii].w;
@@ -74,7 +88,7 @@ vector<Edge> ImpliedEdges(Edge guess, const vector<player_t>& players){
             impl_edges.push_back(make_pair(ww,tt));
             impl_edges.push_back(make_pair(nn,ww));
         } else if (rr == ww && ss == nn){
-            impl_edges.push_back(make_pair(tt,ww))
+            impl_edges.push_back(make_pair(tt,ww));
             impl_edges.push_back(make_pair(tt,nn));
         } else if (rr == nn && ss == ww){
             impl_edges.push_back(make_pair(ww,tt));
@@ -84,18 +98,26 @@ vector<Edge> ImpliedEdges(Edge guess, const vector<player_t>& players){
     return impl_edges;
 }
 
-void AddImpliedEdges(graph_t& F, Edge guess, const vector<player_t>& players){
+void AddImpliedEdges(graph_t& F, Edge guess, const vector<player_t>& players, unsigned short depth){
     queue<Edge> Q;
-    Q.push_back(guess);
+    Q.push(guess);
+    int rr, ss;
+    SPACES(depth);
+    OUTPUT(("guessing %d -> %d\n",guess.first, guess.second));
     while (! Q.empty()){
-        Edge next = Q.pop_front();
-        rr = next.first();
-        ss = next.second();
-        if (add_edge(rr,ss,F).second){ //second gives true if edge is new
+        Edge next = Q.front(); Q.pop();
+        rr = next.first;
+        ss = next.second;
+        SPACES(depth);
+        OUTPUT(("%d -> %d is an edge\n",rr,ss));
+        auto edge_exists = boost::lookup_edge(rr,ss,F);
+        if (! edge_exists.second){ //second gives true if edge exists
+            add_edge(rr,ss,F);
             vector<Edge> impl_edges = ImpliedEdges(next, players);
             for (Edge new_edge : impl_edges){
-                if (add_edge(new_edge.first, new_edge.second, F).second){
-                    Q.push_back(new_edge);
+                if (! edge(new_edge.first, new_edge.second, F).second){
+                    add_edge(new_edge.first, new_edge.second, F);
+                    Q.push(new_edge);
                 }
             }
         }
@@ -106,42 +128,55 @@ void AddImpliedEdges(graph_t& F, Edge guess, const vector<player_t>& players){
  *      players is empty -- all edges are in G (success case)
  *      G contains a cycle -- this says we made a mistake and must backtrack (failure case)
  */
-pair<bool,graph_t> MakeAssumption(graph_t& G, vector<player_t> players){
+pair<bool,graph_t> MakeAssumption(graph_t& G, vector<player_t> players, unsigned short depth){
+    SPACES(depth);
+    OUTPUT(("top of MakeAssumption\n"));
+    SPACES(depth);
+    OUTPUT(("still %d players left in list", (int) players.size()));
     if (players.size() == 0){
-        return make_pair(is_acyclic(G), G); //success base case!
+        auto ret_val = make_pair(is_acyclic(G), G);
+        SPACES(depth);
+        OUTPUT(("G is %scyclic\n",ret_val.first ? "a" : ""));
+        return ret_val; //base case!
     }
     if (! is_acyclic(G)){ //we don't have to go any farther -- already a contradiction
+        SPACES(depth);
+        OUTPUT(("G is cyclic!\n"));
         return make_pair(false, G);
     }
     player_t player = players.back(); //don't forget to pop_back before recursing.
     //(we don't want to pop_back yet because AddImpliedEdges needs to add edge (player.w, player.n)
-    graph_t F(G); //make a copy so as to not mess up the original if this guess is wrong
-    AddImpliedEdges(F, make_pair(player.p, player.w), players);
+    SPACES(depth);
+    OUTPUT(("Assuming %d -> %d\n",player.p,player.w));
+    graph_t F = G; //make a copy so as to not mess up the original if this guess is wrong
+
+    AddImpliedEdges(F, make_pair(player.p, player.w), players, depth);
     players.pop_back();
-    pair<bool, graph_t> result = MakeAssumption(F, players);
+    pair<bool, graph_t> result = MakeAssumption(F, players, depth+1);
     if (result.first){ //if F is complete and acyclic!
-        return make_pair(true,F);
+        return result;
     }
     F = G; //that didn't work... try the other way
-    players.push_back(player) //give AddImpliedEdges the context to add edge (player.n,player.w)
-    AddImpliedEdges(F, make_pair(player.p, player.w), players);
+    players.push_back(player); //give AddImpliedEdges the context to add edge (player.n,player.w)
+    SPACES(depth);
+    OUTPUT(("Assuming %d -> %d\n",player.w,player.p));
+    AddImpliedEdges(F, make_pair(player.w, player.p), players, depth);
     players.pop_back();
-    return MakeAssumption(F, players); //Success or failure, this is our last shot.
+    return MakeAssumption(F, players, depth+1); //Success or failure, this is our last shot.
 }
 
 int main()  
 {
-    int p[] = {1,2,3,4,5,6};
-    int w[] = {2,3,2,2,1,3};
-    int n[] = {3,4,1,1,4,5};
-    vector<player_t> players();
-    for (int i = 0; i < 6; ++i){
-        player_t this_player = {p[i], w[i], n[i] };
-        players.push_back(this_player);
+    int n;
+    cin >> n;
+    vector<player_t> players(n);
+    for(int i=0; i < n; i++){
+        cin >> players[i].p >> players[i].w >> players[i].n;
     }
+   
     vector<int> ordering = FindOrdering(players);
-    for( vector<int>::const_iterator i = ordering.begin(); i != ordering.end(); ++i)
-            std::cout << *i << ' ';
+    for(int i = ordering.size() - 1; i >= 0 ; i-- )
+            std::cout << ordering[i] << ' ';
     cout << endl;
     return 0;
 }

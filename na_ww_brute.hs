2087ef4bfdd2a3148df69d8f5745eeadc90202ea
Brian Schiller <bgschiller@gmail.com>
2013-05-23 22:29:31 -0700

    removed permutations definition, replaced with import from Data.List

--- na_ww_brute.hs
+++ na_ww_brute.hs
@@ -1,18 +1,4 @@
-import Data.List (elemIndex)
- 
--- | The 'permutations' function returns the list of all permutations of the argument.
---
--- > permutations "abc" == ["abc","bac","cba","bca","cab","acb"]
-permutations            :: [a] -> [[a]]
-permutations xs0        =  xs0 : perms xs0 []
-  where
-    perms []     _  = []
-    perms (t:ts) is = foldr interleave (perms ts (t:is)) (permutations is)
-      where interleave    xs     r = let (_,zs) = interleave' id xs r in zs
-            interleave' _ []     r = (ts, r)
-            interleave' f (y:ys) r = let (us,zs) = interleave' (f . (y:)) ys r
-                                     in  (y:us, f (t:y:us) : zs)
- 
+import Data.List (elemIndex, permutations)
  
 {-players is a list of the players.
 permutations is a list of triplets, one for each player p: 
